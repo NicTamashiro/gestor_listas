40efe9a59da71e83e8a9df85424cf8c4b3f4740b
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-05-21 19:04:06 -0300

    atualizando .h

--- gestor_listas.h
+++ gestor_listas.h
@@ -1,4 +1,22 @@
+#ifndef GESTOR_LISTAS_H
+#define GESTOR_LISTAS_H
+ 
+void limpar_tela();
+void limpar_buffer();
+void aguardar();
+ 
 void linhas(char ch, int tamanho);
-void cabecalho(char * texto);
+void cabecalho(char *texto);
+ 
 int ler_inteiro();
+ 
+void criar_lista();
+void remover_lista();
+void renomear_lista();
+void adicionar_item();
+void remover_item();
+void ver_lista();
+ 
 int menu();
+ 
+#endif
